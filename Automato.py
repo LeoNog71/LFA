4538653cb8a2d4442fa2a504f20f12812137b49e
nogueira <leosil.2015@alunos.utfpr.edu.br>
2018-04-10 22:51:07 -0300

    testes feitos

--- Automato.py
+++ Automato.py
@@ -1,7 +1,10 @@
-from pygraph.classes.graph import *
+from pygraph.classes.digraph import *
 estados = ["q0","q1","q2"]
-grafo = graph()
+grafo = digraph()
 grafo.add_nodes(estados)
 print(grafo.nodes())
 grafo.add_edge(("q0","q1"))
+print(grafo.edges())
+grafo.add_edge(("q0","q2"))
+
 print(grafo.edges())
