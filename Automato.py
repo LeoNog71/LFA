dc8a25c3753b54d2a1b89efc66a4941c5dcb648e
nogueira <leosil.2015@alunos.utfpr.edu.br>
2018-04-11 00:58:01 -0300

    primeira versão

--- Automato.py
+++ Automato.py
@@ -1,10 +1,43 @@
 from pygraph.classes.digraph import *
-estados = ["q0","q1","q2"]
-grafo = digraph()
-grafo.add_nodes(estados)
-print(grafo.nodes())
-grafo.add_edge(("q0","q1"))
-print(grafo.edges())
-grafo.add_edge(("q0","q2"))
+from pygraph.algorithms.searching import *
 
-print(grafo.edges())
+string = "aabb"#input()
+
+estados = ["q0","q1","q2","q3"]
+estado_final = ["q0","q3"]
+estado_inicial = "q0"
+alfabeto = ["a","b"]
+
+grafo = digraph()
+
+grafo.add_nodes(estados)
+
+grafo.add_edge(("q0","q1"),label= alfabeto[0])
+grafo.add_edge(("q1","q0"),label = alfabeto[0])
+grafo.add_edge(("q1","q3"),label= alfabeto[1])
+grafo.add_edge(("q0","q2"),label=alfabeto[1])
+grafo.add_edge(("q2","q3"),label=alfabeto[1])
+grafo.add_edge(("q3","q2"),label= alfabeto[1])
+
+estado_atual = estado_inicial
+T = False
+while not T:
+
+    for x in string:
+
+        a = grafo.neighbors(estado_atual)
+
+        for y in a:
+            if grafo.edge_label((estado_atual,y)) == x:
+                print(estado_atual+"=>"+x)
+                estado_atual = y
+                print("=>"+y)
+
+
+
+
+    if estado_atual == estado_final[0] or estado_atual == estado_final[1]:
+        T = True
+    else:
+        print("Sentença nao pertence a linguagem")
+        quit()
